9ddbdebe7c1efc5030d39b4f1295a990b4616f84
pancho <plabradorjr@gmail.com>
2019-11-29 00:11:05 -0600

    minor renaming

--- lib/crypto_prices/cli.rb
+++ lib/crypto_prices/cli.rb
@@ -1,6 +1,6 @@
 class CLI
 
-  def call
+  def run
     puts "Welcome! Here are today's top 10 cryptocurrencies"
     # add method to ask user input to show more details
     # add another method to exit
